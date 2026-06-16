82337fc84e2e0cb1f3b1bb0d8afde68ade939ac5
Daehwi90 <eognl101@gmail.com>
2026-06-16 16:44:11 +0900

    변경2

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ if st.button("Send balloons!"):
 
 
 import pandas as pd
-q
 import numpy as np
 
 st.write("Streamlit supports a wide range of data visualizations, including [Plotly, Altair, and Bokeh charts](https://docs.streamlit.io/develop/api-reference/charts). 📊 And with over 20 input widgets, you can easily make your data interactive!")
