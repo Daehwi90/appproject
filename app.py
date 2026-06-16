d1052fe421e45546a233b6d2cb13e18698823802
Daehwi90 <eognl101@gmail.com>
2026-06-16 16:44:17 +0900

    변경2

--- app.py
+++ app.py
@@ -17,10 +17,6 @@ if st.button("Send balloons!"):
     st.balloons()
 
 
-
-import pandas as pd
-import numpy as np
-
 st.write("Streamlit supports a wide range of data visualizations, including [Plotly, Altair, and Bokeh charts](https://docs.streamlit.io/develop/api-reference/charts). 📊 And with over 20 input widgets, you can easily make your data interactive!")
 
 all_users = ["Alice", "Bob", "Charly"]
